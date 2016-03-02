4db31dce0ba851ef35ab5da09f1b78feefe98be6
Romain Soubrane <r.soubrane@gmail.com>
2016-03-02 04:46:24 +0000

    0.2

--- StartUpProject/Classes/Menu/My Profile/ProfileViewControllerTableViewController.swift
+++ StartUpProject/Classes/Menu/My Profile/ProfileViewControllerTableViewController.swift
@@ -7,18 +7,113 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
 
 class ProfileViewController: UITableViewController {
 
+    //Link the Labels with the Storyboard
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var idLabel: UILabel!
+    @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    @IBOutlet weak var profilePicture: UIImageView!
+    
+    //Initialize the Labels
+    var facebookid : String!
+    var username : String!
+    var userEmail : String!
+    var gender : String!
+    var birthday : String!
+    
+    @IBOutlet weak var menuButton: UIBarButtonItem!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
 
+        //Check if the Sidebar is active
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+            
+            //Enable the Gestures and prevent from selecting a cell
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            view.removeGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+            
+            //Set the title
+            title = "Profile Page"
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Hide the navigation bar if the Sidebar is on
+        navigationController?.hidesBarsOnSwipe = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+        
+        //This permits to unselect the previously selected cell when going back to table view
+        let indexPath = self.tableView.indexPathForSelectedRow
+        if (indexPath != nil) {
+            self.tableView.deselectRowAtIndexPath(indexPath!, animated: animated)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
+    //Return the Data
+    func returnUserData() {
+            
+        let graphRequest : FBSDKGraphRequest = FBSDKGraphRequest(graphPath:  "me", parameters: ["fields":"email, name, gender, birthday"])
+        graphRequest.startWithCompletionHandler({ (connection, result, error) -> Void in
+            
+        if ((error) != nil) {
+        // Process error
+        print("Error: \(error)")
+        } else {
+            self.facebookid = (result.valueForKey("id") as? String)!
+            self.username = (result.valueForKey("name") as? String)!
+            self.userEmail = (result.valueForKey("email") as? String)!
+            self.gender = (result.valueForKey("gender") as? String)!
+            self.birthday = (result.valueForKey("birthday") as? String)!
+        
+            self.nameLabel.text = self.username
+            self.emailLabel.text = self.userEmail
+            self.idLabel.text = self.facebookid
+            self.genderLabel.text = self.gender
+            self.ageLabel.text = self.birthday
+        
+            self.load_image("https://graph.facebook.com/\(self.facebookid)/picture?type=large", imageLabel: self.profilePicture)
+            }
+        })
+    }
+    
+    //Function that will load the Profile Image
+    func load_image(urlString:String, imageLabel: UIImageView) {
+        let imgURL: NSURL = NSURL(string: urlString)!
+        let request: NSURLRequest = NSURLRequest(URL: imgURL)
+    
+        let session = NSURLSession.sharedSession()
+        let task = session.dataTaskWithRequest(request) {
+            (data, response, error) -> Void in
+            
+            if (error == nil && data != nil) {
+                func display_image() {
+                    imageLabel.image = UIImage(data: data!)
+                }
+                dispatch_async(dispatch_get_main_queue(), display_image)
+            }
+        }
+        task.resume()
+    }
+    
+    
+    
+    
     // MARK: - Table view data source
 
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
---
+++ StartUpProject/Classes/Menu/Settings/Settings.swift
@@ -0,0 +1,65 @@
+//
+//  Settings.swift
+//  StartUpProject
+//
+//  Created by Romain on 02/03/2016.
+//  Copyright © 2016 Romain Soubrane. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+
+class Settings: UIViewController {
+    
+    @IBOutlet weak var menuButton:UIBarButtonItem!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        //Check if the Sidebar is active
+        if revealViewController() != nil {
+            menuButton.target = revealViewController()
+            menuButton.action = "revealToggle:"
+    
+            //Enable the Gestures and prevent from selecting a cell
+            view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            view.removeGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    
+            //Set the title
+            title = "Profile Page"
+        }
+    }
+    
+    //Function to reset the data when Button is clicked
+    @IBAction func resetData(sender: AnyObject) {
+        resetDisplayWalkthroughs()
+        let loginManager = FBSDKLoginManager()
+        loginManager.logOut()
+    }
+    
+
+    //Func to reset the Walkthrough
+    func resetDisplayWalkthroughs() {
+    
+    // Force to reset the walkthroughs
+    _ = NSUserDefaults.standardUserDefaults()
+    let displayedWalkthrough = false
+    
+    let userDefaults = NSUserDefaults.standardUserDefaults()
+    userDefaults.setBool(false, forKey: "DisplayedWalkthrough")
+    
+    // let's show them
+    if !displayedWalkthrough {
+    // instantiate neew PageVC via storyboard
+    if let pageViewController = storyboard?.instantiateViewControllerWithIdentifier("PageViewController") as? PageViewController {
+    self.presentViewController(pageViewController, animated: true, completion: nil)
+    }
+    }
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
